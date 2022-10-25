00489e721e4b28d075909323357a55dc78bdd63f
xjeline6 <xjeline6@fi.muni.cz>
2022-10-25 18:09:24 +0200

    resize images also as a script

---
+++ util/resize_images.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import os
+import pyvips
+
+print(os.getcwd())
+destination_folder = Path('/home/tomas/Projects/histoseg/data/Feit_colon-annotation')
+
+os.chdir(destination_folder)
+
+
+FACTOR = 2
+
+tiff_paths = Path(os.getcwd()).rglob('*.tiff')
+
+for tiff_path in tiff_paths:
+    image_name = tiff_path.stem
+    new_name = Path(str(image_name) + '_small.tiff')
+    new_path = tiff_path.parent / new_name
+
+    print(new_path)
+    image = pyvips.Image.tiffload(str(tiff_path), page=FACTOR)
+    image.tiffsave(str(new_path), bigtiff=True)
