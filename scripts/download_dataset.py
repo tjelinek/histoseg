153a62d27a49d8657e138b0a3356415193ff8692
tjelinek <tomasjelinek96@gmail.com>
2022-12-16 12:17:51 +0100

    changed order

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,10 +1,10 @@
-from util.dataset_processing import fetch_files_prof_feit
-from pathlib import Path
-from util.data_manipulation_scripts import decompress_bz2_in_subfolder
-
 import sys
 from os.path import dirname
 sys.path.append(dirname(__file__))
+
+from util.dataset_processing import fetch_files_prof_feit
+from pathlib import Path
+from util.data_manipulation_scripts import decompress_bz2_in_subfolder
 
 destination_folder = Path('/home/tomas/Projects/histoseg/data/Feit_colon-annotation')
 fetch_files_prof_feit('https://atlases.muni.cz/atlases/colonannot.html', destination_folder)
