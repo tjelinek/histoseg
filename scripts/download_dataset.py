5749e322ea10ce00a0f1c6d5c5c34787383c89f1
tjelinek <tomasjelinek96@gmail.com>
2022-12-16 12:17:12 +0100

    home dir added

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -2,6 +2,10 @@ from util.dataset_processing import fetch_files_prof_feit
 from pathlib import Path
 from util.data_manipulation_scripts import decompress_bz2_in_subfolder
 
+import sys
+from os.path import dirname
+sys.path.append(dirname(__file__))
+
 destination_folder = Path('/home/tomas/Projects/histoseg/data/Feit_colon-annotation')
 fetch_files_prof_feit('https://atlases.muni.cz/atlases/colonannot.html', destination_folder)
 
